c6fb10d733b954326df825b1b5363587142baecd
Yashwanth <yasw2875@colorado.edu>
2024-03-30 00:03:42 -0600

    feat[UserService] : base app with express server

---
+++ UserService/src/main.ts
@@ -0,0 +1,26 @@
+import bodyParser from "body-parser";
+import cors from 'cors';
+import express from "express";
+
+const port = 8080;
+const app = express();
+
+function checkErrors(err: any, req: any, res: any, next: any) {
+  if (err instanceof SyntaxError && err.message.indexOf("JSON")) {
+    res.status(400).json({ errorCode: 100, Ref: "Error parsing JSON" });
+  } else {
+    res.status(400).json({ errorCode: 101, Ref: "Error parsing JSON" });
+  }
+}
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(checkErrors);
+
+async function main() {
+    app.listen(port);
+}
+
+main();
+export { app };
