8bf605bd6a8ee216ef1b382d569f08a0f2ed8d75
Yashwanth <yasw2875@colorado.edu>
2024-04-01 13:33:44 -0600

    feat[Frontend] : page structure

---
+++ frontend/src/pages/index.jsx
@@ -0,0 +1,4 @@
+export { default as Calender } from './Calender';
+export { default as Companies } from './Companies';
+export { default as Dashboard } from './Dashboard';
+export { default as Mail } from './Mail';
