7dd03acced1dfe7f9350639356d02123abc54d4c
Yashwanth <yasw2875@colorado.edu>
2024-04-01 13:52:50 -0600

    feat[Frontend] : update the component rendering for app along with routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,10 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { FiSettings } from 'react-icons/fi';
 import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 
-import './App.css'
+import { Navbar, Footer, Sidebar, ThemeSettings } from './components';
+import { Calender, Companies, Dashboard, Mail, Portfolio } from './pages';
+import './App.css';
+
 const App = () => {
   const activeMenu = true;
   return (
@@ -21,26 +24,25 @@ const App = () => {
           </div>
           {activeMenu ? (
             <div className='w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white'>
-              Sidebar
+              <Sidebar />
             </div>
           ) : (
             <div className='w-0 dark:bg-secondary-dark-bg'>
-              Sidebar w-0
+              <Sidebar />
             </div>
           )}
-          <div className={`dark:bg-main-bg bg-main-bg min-h-screen w-full ${activeMenu ? 'md:ml-72': 'flex-2'}`}>
+          <div className={`dark:bg-main-bg bg-main-bg min-h-screen w-full ${activeMenu ? 'md:ml-72' : 'flex-2'}`}>
             <div className='fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full'>
-              Navbar
+              <Navbar />
             </div>
           </div>
           <div>
             <Routes>
-              <Route path='/' elements="Dashboard"/>
-              <Route path='/notification' elements="Notifications"/>
-              <Route path='/companies' elements="MyCompanines"/>
-              <Route path='/portfolio' elements="Portfolio"/>
-              <Route path='/calender' elements="Calender"/>
-              <Route path='/mail' elements="Mail" />
+              <Route path='' elements={<Dashboard />} />
+              <Route path='/companies' elements={<Companies />} />
+              <Route path='/portfolio' elements={<Portfolio />} />
+              <Route path='/calender' elements={<Calender />} />
+              <Route path='/mail' elements={<Mail />} />
             </Routes>
           </div>
         </div>
--- frontend/src/pages/index.jsx
+++ frontend/src/pages/index.jsx
@@ -2,3 +2,4 @@ export { default as Calender } from './Calender';
 export { default as Companies } from './Companies';
 export { default as Dashboard } from './Dashboard';
 export { default as Mail } from './Mail';
+export { default as Portfolio } from './Portfolio';
