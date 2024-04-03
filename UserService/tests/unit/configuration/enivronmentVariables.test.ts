f4b72051fa70c9b0381b741015dd4eb74da04656
Yashwanth <yasw2875@colorado.edu>
2024-04-03 16:41:45 -0600

    feat[UserService]: dockerising user service

--- UserService/tests/unit/configuration/enivronmentVariables.test.ts
+++
@@ -1,9 +0,0 @@
-import { expect } from "chai";
-import { get } from "env-var";
-import sinon from "sinon";
-import { environmentVariables } from "../../../src/configuration/environmentVariables";
-
-describe("environmentVariables", () => {
-    it("should be an instance of EnvironmentVariables", () => {
-    });
-});
